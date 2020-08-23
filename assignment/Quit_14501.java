91a3b6fea342ce0a69b637c225da553c0e29082f
yuhyeon <klyhyeon@gmail.com>
2020-08-24 00:17:39 +0900

    #4_week_personal_quit(edit)
    퇴사일 코드 수정

--- assignment/Quit_14501.java
+++ assignment/Quit_14501.java
@@ -21,85 +21,37 @@ package baekJoon.week4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 
 public class Quit_14501 {
     public static void main(String[] args) throws IOException {
-
-        //GG
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        //n일 = day
-//        int day = Integer.parseInt(br.readLine());
-//        //profit 담을 변수선언
-//        int profitSum = 0;
-//        //2차원 배열 선언
-//        int[][] argArr = new int[day][2];
-//        //기간Ti와 Pi n번 반복 입력
-//        for(int i = 0; i < day; i++) {
-//            String[] arg = br.readLine().split(" ");
-//            argArr[i][0] = Integer.parseInt(arg[0]);
-//            argArr[i][1] = Integer.parseInt(arg[1]);
-//        }
-//
-//
-//        //조건1) 기간이 n일을 넘지 말아야 한다.
-//        for(int j = 0; j < day; j++) {
-//            //종료시간
-//            int endDate = argArr[j][0] + j;
-//            argArr[j][0] = endDate;
-//        }
-//        //종료시간 기준 오름차순 정렬
-////        Arrays.sort(argArr, new Comparator<int[]>() {
-////            @Override
-////            public int compare(int[] o1, int[] o2) {
-////                return o1[0] - o2[0];
-////            }
-////        });
-//
-//        for (int i = 0; i < day; i++) {
-//            System.out.println(argArr[i][0] + " " + argArr[i][1]);
-//        }
-//
-//        //조건2) 최대수익 구하자.
-//        //중복되면 더 높은 값 선택
-//        //종료시간 n일 넘는 값은 제거
-//        for (int k = 0; k < day; k++) {
-//
-//
-//
-//        }
-//
-//        System.out.println(profitSum);
-//
-//
-//
-        //풀이
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int day = Integer.parseInt(br.readLine());
 
-        int[] time = new int[day];
-        int[] profit = new int[day];
-        int[] profitMax = new int[day];
-        int max = 0;
-        String[] str;
-
-        for(int i = 1; i <= day; i++) {
-            str = br.readLine().split(" ");
-            time[i] = Integer.parseInt(str[0]);
-            profit[i] = Integer.parseInt(str[1]);
+        //n받음
+        int date = Integer.parseInt(br.readLine());
+        //dp배열 선언
+        int[] dp = new int[date+15];
+        int[] time = new int[date+15];
+        int[] profit = new int[date+15];
+        //time, profit 입력
+        for(int i = 1; i <= date; i++) {
+            String[] val = br.readLine().split(" ");
+            time[i] = Integer.parseInt(val[0]);
+            profit[i] = Integer.parseInt(val[1]);
+            val = null;
         }
-        //입력부
-
-        for(int j = 1; j <= day; j++) {
-            int workingTime = j + time[j];
-
-            if(workingTime <= day) {
-                profitMax[j] = Math.max(profit[j] + profitMax[workingTime], profitMax[j+1]);
+        //dp 구문
+        for(int j = date; j > 0; j--) {
+            int days = time[j] + j;
+            if(days > date+1) {
+                //상담기간이 퇴사일을 넘기면 다음날 스케쥴로 진행
+                dp[j] = dp[j+1];
             } else {
-
+                //max(스케쥴을 진행하지 않았을 때 최대값, 진행했을 때 최대값(수익+진행했을 때 다음 스케쥴의 최대값)
+                dp[j] = Math.max(dp[j+1], dp[days]+profit[j]);
             }
         }
-        System.out.println(max);
+        //정답 출력: dp[0]: 1(0)일부터 퇴사일까지 상담했을 시 최대값
+        System.out.println(dp[1]);
+
     }
 }
